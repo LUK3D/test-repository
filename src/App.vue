0873c6bb70288682f798b652c33c71c057213a17
Filipe Lukebana <filipelukebana@gmail.com>
2022-12-10 19:32:18 -0300

    🚀 First commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+  <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit non minus iste quis, libero quas quasi esse
+    tempore laborum deleniti consequatur assumenda ex nemo quod voluptatem architecto impedit fugit repellat!</p>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
